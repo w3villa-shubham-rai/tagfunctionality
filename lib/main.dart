941a97a492cdc86d4b9964967e0395a3e89f748f
w3villa-shubham-rai <shubham.rai@w3villa.com>
2023-12-20 17:57:03 +0530

    working on tag people

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,36 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mentions/flutter_mentions.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      builder: (_, child) => Portal(child: child!),
+      builder: (_, child) => 
+      Portal(child: child!),
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
+  const MyHomePage({Key? key, this.title}) : super(key: key);
 
   final String? title;
 
   @override
+  
   _MyHomePageState createState() => _MyHomePageState();
 }
 
@@ -34,41 +39,31 @@ class _MyHomePageState extends State<MyHomePage> {
 
   List<Map<String,dynamic>> data=[         
                       {
-                        'id': '61as61fsa',
-                        'display': 'fayeedP',
-                        'full_name': 'Fayeed Pawaskar',
+                        'id': '61ass61fsa',
+                        'display': 'Shubham',
                         'photo':
                             'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
                       },
                       {
-                        'id': '61asasgasgsag6a',
-                        'display': 'khaled',
-                        'full_name': 'DJ Khaled',
-                        'style': TextStyle(color: Colors.purple),
+                        'id': '61asasssgasgsag6a',
+                        'display': 'diwakr',
+                        'style': const TextStyle(color: Colors.purple),
                         'photo':
                             'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
                       },
                       {
-                        'id': 'asfgasga41',
-                        'display': 'markT',
-                        'full_name': 'Mark Twain',
+                        'id': 'asfgascga41',
+                        'display': 'Aman',
                         'photo':
                             'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
                       },
                       {
-                        'id': 'asfsaf451a',
-                        'display': 'JhonL',
-                        'full_name': 'Jhon Legend',
+                        'id': 'asfsaf45ww1a',
+                        'display': 'chiku',
                         'photo':
                             'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
                       },
      ];
-                 
-  
-
-
-
-
 
 
   @override
@@ -77,58 +72,95 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title!),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          ElevatedButton(
-            child: Text('Get Text'),
-            onPressed: () {
-              print(key.currentState!.controller!.markupText);
-            },
-          ),
-          Container(
-            child: FlutterMentions(
-              key: key,
-              suggestionPosition: SuggestionPosition.Top,
-              maxLines: 5,
-              minLines: 1,
-              decoration: InputDecoration(hintText: 'hello'),
-              mentions: [
-                Mention(
-                    trigger: '@',
-                    style: TextStyle(
-                      color: Colors.blue,
-                    ),
-                    data: data,
-                  matchAll: false,
-                    suggestionBuilder: (data) {
-                      return Container(
-                        padding: EdgeInsets.all(10.0),
-                        child: Row(
-                          children: <Widget>[
-                            CircleAvatar(
-                              backgroundImage: NetworkImage(
-                                data['photo'],
-                              ),
-                            ),
-                            SizedBox(
-                              width: 20.0,
-                            ),
-                            Column(
-                              children: <Widget>[
-                                Text(data['full_name']),
-                                Text('@${data['display']}'),
-                              ],
-                            )
-                          ],
-                        ),
-                      );
-                    }),
-              ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: const Text('Get Text'),
+              onPressed: () {
+                if (kDebugMode) {
+                  print(key.currentState!.controller!.markupText);
+                }
+              },
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Container(
+                 height: 50,
+                  decoration:  BoxDecoration(
+                    color: Color(0xFFF0F0F0),
+                    borderRadius:
+                        BorderRadius.all(Radius.circular(9)),
+                    border: Border.all(width: 1, color: Colors.white),
+                  ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8),
+                  child: FlutterMentions(
+                    key: key,
+                    suggestionPosition: SuggestionPosition.Top,
+                    maxLines: 5,
+                    minLines: 1,
+                    decoration:InputDecoration
+                          (
+                            border: InputBorder.none,
+                            hintText: 'Write your comment',
+                            suffixIcon: IconButton(
+                              onPressed: () 
+                              {                        
+                              
+                              },
+                              icon: Icon(Icons.send),
+                            ),
+                          ),
+                            
+                      
+                    mentions: [
+                      Mention(
+                          trigger: '@',
+                          style: const TextStyle(
+                            color: Colors.blue,
+                          ),
+                          data: data,
+                        matchAll: false,
+                          suggestionBuilder: (data) {
+                            return Container(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Row(
+                                children: <Widget>[
+                                  CircleAvatar(
+                                    backgroundImage: NetworkImage(
+                                      data['photo'],
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 20.0,
+                                  ),
+                                  Row(
+                                    children: <Widget>[
+                                      // Text(data['full_name']),
+                                        CircleAvatar(
+                                    backgroundImage: NetworkImage(
+                                      data['photo'],
+                                    ),
+                                    radius: 5,
+                                  ),
+                                      Text('${data['display']}'),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            );
+                          }),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+       
