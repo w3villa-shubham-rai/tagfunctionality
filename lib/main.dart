857c0356197384090fe309b3cfebb21f55f0b860
shubham <shubham.rai@w3villa.com>
2024-01-05 14:04:57 +0530

    working on show tag data from api

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mentions/flutter_mentions.dart';
+import 'package:http/http.dart' as http;
+
 
 void main() {
   runApp(const MyApp());
@@ -36,34 +40,62 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   GlobalKey<FlutterMentionsState> key = GlobalKey<FlutterMentionsState>();
+  List<UserProfile> data = [];
 
-  List<Map<String,dynamic>> data=[         
-                      {
-                        'id': '61ass61fsa',
-                        'display': 'Shubham',
-                        'photo':
-                            'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-                      },
-                      {
-                        'id': '61asasssgasgsag6a',
-                        'display': 'diwakr',
-                        'style': const TextStyle(color: Colors.purple),
-                        'photo':
-                            'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-                      },
-                      {
-                        'id': 'asfgascga41',
-                        'display': 'Aman',
-                        'photo':
-                            'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-                      },
-                      {
-                        'id': 'asfsaf45ww1a',
-                        'display': 'chiku',
-                        'photo':
-                            'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-                      },
-     ];
+    @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+ Future<void> fetchData() async {
+    const String token =
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoyLCJ0aW1lIjoxNzAxOTQ0NjE1fQ.ZCl1pDug6j90L4HqVcCjNMSYF3wRuRac1gy9XPUyXZY';
+      final response = await http.get(Uri.parse('https://staging.simmpli.com/api/v1/profiles/mentions.json?q='),headers: {'Authorization': token});
+
+
+   
+    if (response.statusCode == 200) {
+      final List<dynamic> responseData = json.decode(response.body);
+      setState(() {
+        data = responseData.map((json) => UserProfile.fromJson(json)).toList();
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+
+
+
+
+  // List<Map<String,dynamic>> data=[         
+  //                     {
+  //                       'id': '61ass61fsa',
+  //                       'display': 'Shubham',
+  //                       'photo':
+  //                           'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  //                     },
+  //                     {
+  //                       'id': '61asasssgasgsag6a',
+  //                       'display': 'diwakr',
+  //                       'style': const TextStyle(color: Colors.purple),
+  //                       'photo':
+  //                           'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  //                     },
+  //                     {
+  //                       'id': 'asfgascga41',
+  //                       'display': 'Aman',
+  //                       'photo':
+  //                           'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  //                     },
+  //                     {
+  //                       'id': 'asfsaf45ww1a',
+  //                       'display': 'chiku',
+  //                       'photo':
+  //                           'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  //                     },
+  //    ];
 
 
   @override
@@ -121,34 +153,37 @@ class _MyHomePageState extends State<MyHomePage> {
                           style: const TextStyle(
                             color: Colors.blue,
                           ),
-                          data: data,
+                           data: data.map((userProfile) => userProfile.toJson()).toList(),
                         matchAll: false,
                           suggestionBuilder: (data) {
-                            return Container(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Row(
-                                children: <Widget>[
-                                  CircleAvatar(
-                                    backgroundImage: NetworkImage(
-                                      data['photo'],
+                            return Center(
+                              child: Container(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Row(
+                                  children: <Widget>[
+                                    CircleAvatar(
+                                      backgroundImage: NetworkImage(
+                                        data['photo'],
+                                      ),
+                                      radius: 10,
                                     ),
-                                  ),
-                                  const SizedBox(
-                                    width: 20.0,
-                                  ),
-                                  Row(
-                                    children: <Widget>[
-                                      // Text(data['full_name']),
-                                        CircleAvatar(
-                                    backgroundImage: NetworkImage(
-                                      data['photo'],
+                                    const SizedBox(
+                                      width: 10.0,
                                     ),
-                                    radius: 5,
-                                  ),
-                                      Text('${data['display']}'),
-                                    ],
-                                  )
-                                ],
+                                    Row(
+                                      children: <Widget>[
+                                        // Text(data['full_name']),
+                                    //       CircleAvatar(
+                                    //   backgroundImage: NetworkImage(
+                                    //     data['photo'],
+                                    //   ),
+                                    //   radius: 5,
+                                    // ),
+                                        Text('${data['display']}'),
+                                      ],
+                                    )
+                                  ],
+                                ),
                               ),
                             );
                           }),
@@ -164,3 +199,37 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
        
+
+
+
+class UserProfile {
+  final int id;
+  final String fullName;
+  final String sgid;
+  final String content;
+
+  UserProfile({
+    required this.id,
+    required this.fullName,
+    required this.sgid,
+    required this.content,
+  });
+
+  factory UserProfile.fromJson(Map<String, dynamic> json) {
+    return UserProfile(
+      id: json['id'] as int,
+      fullName: json['full_name'] as String,
+      sgid: json['sgid'] as String,
+      content: json['content'] as String,
+    );
+  }
+
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id.toString(),
+      'display': fullName, // Adjust accordingly based on your data structure
+      'photo': content, // Adjust accordingly based on your data structure
+    };
+  }
+}
