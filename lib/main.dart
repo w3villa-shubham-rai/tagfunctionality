de75b6a454f4ed6b05a68b4b1f1519c09d51859a
shubham <shubham.rai@w3villa.com>
2024-01-10 15:59:29 +0530

    i found id

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mentions/flutter_mentions.dart';
 import 'package:http/http.dart' as http;
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -17,8 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      builder: (_, child) => 
-      Portal(child: child!), 
+      builder: (_, child) => Portal(child: child!),
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -35,7 +33,6 @@ class MyHomePage extends StatefulWidget {
   final String? title;
 
   @override
-  
   _MyHomePageState createState() => _MyHomePageState();
 }
 
@@ -43,97 +40,65 @@ class _MyHomePageState extends State<MyHomePage> {
   GlobalKey<FlutterMentionsState> key = GlobalKey<FlutterMentionsState>();
   List<UserProfile> data = [];
   String mentionQuery = "";
-  int count=1;
-    @override
+  String userText = "";
+  List<int> mentionedUserIds = [];
+  int count = 1;
+
+  @override
   void initState() {
-    super.initState();    
-     
+    super.initState();
+
     conditionFunction();
   }
 
-void conditionFunction() {
-  if (count == 1) {
-    fetchDataWithoutQuery();
-  } else {
-    fetchDataWithQuery(mentionQuery);
+  void conditionFunction() {
+    if (count == 1) {
+      fetchDataWithoutQuery();
+    } else {
+      fetchDataWithQuery(mentionQuery);
+    }
   }
-}
 
-Future<void> fetchDataWithoutQuery() async {
-  const String token =
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoyLCJ0aW1lIjoxNzAxOTQ0NjE1fQ.ZCl1pDug6j90L4HqVcCjNMSYF3wRuRac1gy9XPUyXZY';
-  final response = await http.get(
-    Uri.parse('https://staging.simmpli.com/api/v1/profiles/mentions.json?q='),
-    headers: {'Authorization': token},
-  );
+  Future<void> fetchDataWithoutQuery() async {
+    const String token =
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoyLCJ0aW1lIjoxNzAxOTQ0NjE1fQ.ZCl1pDug6j90L4HqVcCjNMSYF3wRuRac1gy9XPUyXZY';
+    final response = await http.get(
+      Uri.parse('https://staging.simmpli.com/api/v1/profiles/mentions.json?q='),
+      headers: {'Authorization': token},
+    );
 
-  print("Response of API: $response");
+    print("Response of API: $response");
 
-  if (response.statusCode == 200) {
-    final List<dynamic> responseData = json.decode(response.body);
-    setState(() {
-      data = responseData.map((json) => UserProfile.fromJson(json)).toList();
-    });
-  } else {
-    throw Exception('Failed to load data');
+    if (response.statusCode == 200) {
+      final List<dynamic> responseData = json.decode(response.body);
+      setState(() {
+        data = responseData.map((json) => UserProfile.fromJson(json)).toList();
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
   }
-}
 
-Future<void> fetchDataWithQuery(String query) async {
-  print('Fetching data for query: $query');
-  const String token =
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoyLCJ0aW1lIjoxNzAxOTQ0NjE1fQ.ZCl1pDug6j90L4HqVcCjNMSYF3wRuRac1gy9XPUyXZY';
-  final response = await http.get(
-    Uri.parse('https://staging.simmpli.com/api/v1/profiles/mentions.json?q=$query'),
-    headers: {'Authorization': token},
-  );
+  Future<void> fetchDataWithQuery(String query) async {
+    print('Fetching data for query: $query');
+    const String token =
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoyLCJ0aW1lIjoxNzAxOTQ0NjE1fQ.ZCl1pDug6j90L4HqVcCjNMSYF3wRuRac1gy9XPUyXZY';
+    final response = await http.get(
+      Uri.parse('https://staging.simmpli.com/api/v1/profiles/mentions.json?q=$query'),
+      headers: {'Authorization': token},
+    );
 
-  print("Response of API: $response");
+    print("Response of API: $response");
 
-  if (response.statusCode == 200) {
-    final List<dynamic> responseData = json.decode(response.body);
-    setState(() {
-      data = responseData.map((json) => UserProfile.fromJson(json)).toList();
-    });
-  } else {
-    throw Exception('Failed to load data');
+    if (response.statusCode == 200) {
+      final List<dynamic> responseData = json.decode(response.body);
+      setState(() {
+        data = responseData.map((json) => UserProfile.fromJson(json)).toList();
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
   }
-}
-
-
-
- 
-
-
-
-  // List<Map<String,dynamic>> data=[         
-  //                     {
-  //                       'id': '61ass61fsa',
-  //                       'display': 'Shubham',
-  //                       'photo':
-  //                           'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-  //                     },
-  //                     {
-  //                       'id': '61asasssgasgsag6a',
-  //                       'display': 'diwakr',
-  //                       'style': const TextStyle(color: Colors.purple),
-  //                       'photo':
-  //                           'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-  //                     },
-  //                     {
-  //                       'id': 'asfgascga41',
-  //                       'display': 'Aman',
-  //                       'photo':
-  //                           'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-  //                     },
-  //                     {
-  //                       'id': 'asfsaf45ww1a',
-  //                       'display': 'chiku',
-  //                       'photo':
-  //                           'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-  //                     },
-  //    ];
-
 
   @override
   Widget build(BuildContext context) {
@@ -150,6 +115,7 @@ Future<void> fetchDataWithQuery(String query) async {
               onPressed: () {
                 if (kDebugMode) {
                   print(key.currentState!.controller!.markupText);
+                  print("Mentioned User IDs: $mentionedUserIds");
                 }
               },
             ),
@@ -157,90 +123,94 @@ Future<void> fetchDataWithQuery(String query) async {
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Center(
                 child: Container(
-                   height: 50,
-                    decoration:  BoxDecoration(
-                      color: Color(0xFFF0F0F0),                      
-                      borderRadius:
-                          BorderRadius.all(Radius.circular(9)),
-                      border: Border.all(width: 1, color: Colors.white),
-                    ),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF0F0F0),
+                    borderRadius: const BorderRadius.all(Radius.circular(9)),
+                    border: Border.all(width: 1, color: Colors.white),
+                  ),
                   child: Padding(
                     padding: const EdgeInsets.only(left: 8),
                     child: FlutterMentions(
                       key: key,
                       suggestionPosition: SuggestionPosition.Top,
-                      maxLines: 5,                      
-                      // onChanged:(value) {
-                        
-                      // },
+                      maxLines: 5,
                       minLines: 2,
-                      decoration:InputDecoration
-                            (
-                              border: InputBorder.none,
-                              hintText: 'Write your comment',
-                              suffixIcon: IconButton(
-                                onPressed: () 
-                                {                        
-                                   
-                                },
-                                icon: Icon(Icons.send),
-                              ),
-                            ),                            
-                        
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Write your comment',
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            if (kDebugMode) {
+                              print(key.currentState!.controller!.markupText);
+                              print("Mentioned User IDs: $mentionedUserIds");
+                              for (int id in mentionedUserIds) {
+                                print("User ID: $id");
+                              }
+                            }
+                          },
+                          icon: Icon(Icons.send),
+                        ),
+                      ),
                       mentions: [
                         Mention(
-                            trigger: '@',
-                            style: const TextStyle(
-                              color: Colors.blue,
-                            ),
-                             data: data.map((userProfile) => userProfile.toJson()).toList(),
+                          trigger: '@',
+                          style: const TextStyle(
+                            color: Colors.blue,
+                          ),
+                          data: data.map((userProfile) => userProfile.toJson()).toList(),
                           matchAll: false,
-                            suggestionBuilder: (data) {
-                                return Center(
-                                child: Container(
-                                  padding:  EdgeInsets.all(10.0),
-                                  decoration:   const BoxDecoration(
-                                    color: Colors.white,
-                                   
-                                  ),
-                                  child: Row(
-                                    children: <Widget>[
-                                      CircleAvatar(
-                                        backgroundImage: NetworkImage(
-                                          data['photo'],
-                                        ),
-                                        radius: 10,
-                                      ),
-                                      const SizedBox(
-                                        width: 10.0,
-                                      ),
-                                      Row(
-                                        children: <Widget>[
-                                          Text('${data['display']}'),
-                                        ],
-                                      )
-                                    ],
-                                  ),
+                          suggestionBuilder: (data) {
+                            return Center(
+                              child: Container(
+                                padding: EdgeInsets.all(10.0),
+                                decoration: const BoxDecoration(
+                                  color: Colors.white,
                                 ),
-                              );
-                            
-                             
-                              
-                              }),
+                                child: Row(
+                                  children: <Widget>[
+                                    CircleAvatar(
+                                      backgroundImage: NetworkImage(
+                                        "https://staging.simmpli.com/" + data['photo'],
+                                      ),
+                                      radius: 10,
+                                    ),
+                                    const SizedBox(
+                                      width: 10.0,
+                                    ),
+                                    Row(
+                                      children: <Widget>[
+                                        Text('${data['display']}'),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
                       ],
-                        onChanged: (text) {
+                      onChanged: (text) {
+                        setState(() {
+                          userText = text;
+                        });
+
                         final query = text.substring(text.lastIndexOf('@') + 1);
                         setState(() {
                           mentionQuery = query;
-                        });                  
-                       
-                          count++;
-                          if (count == 1) {
-                            fetchDataWithoutQuery();
-                          } else {
-                            fetchDataWithQuery(mentionQuery);
-                          }
-                        
+                        });
+
+                        count++;
+                        if (count == 1) {
+                          fetchDataWithoutQuery();
+                        } else {
+                          fetchDataWithQuery(mentionQuery);
+                        }
+
+                        // Extract mentioned user IDs from userText using the model
+                        List<int> newMentionedUserIds = extractUserIdsFromModel(userText);
+                        mentionedUserIds.clear();
+                        mentionedUserIds.addAll(newMentionedUserIds);
                       },
                     ),
                   ),
@@ -252,10 +222,17 @@ Future<void> fetchDataWithQuery(String query) async {
       ),
     );
   }
+
+  List<int> extractUserIdsFromModel(String text) {
+    Set<int> uniqueIds = Set<int>();
+    for (UserProfile userProfile in data) {
+      if (text.contains('@${userProfile.fullName}')) {
+        uniqueIds.add(userProfile.id);
+      }
+    }
+    return List<int>.from(uniqueIds);
+  }
 }
-       
-
-
 
 class UserProfile {
   final int id;
@@ -279,12 +256,13 @@ class UserProfile {
     );
   }
 
-
   Map<String, dynamic> toJson() {
     return {
       'id': id.toString(),
-      'display': fullName, 
-      'photo': content, 
+      'display': fullName,
+      'photo': content,
     };
   }
 }
+
+
